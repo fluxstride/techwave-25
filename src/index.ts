a31cbf5fde22ede93d2cee6a3a1f986c54527e4b
Samuel Adekoya <samueladexx@gmail.com>
2025-06-03 08:31:49 +0100

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://techwave-25.vercel.app" }));
 
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Welcome to TechWave '25 API" });
